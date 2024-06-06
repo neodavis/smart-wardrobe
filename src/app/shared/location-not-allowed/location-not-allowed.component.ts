fa96e5ef2cf3ff187e646f9b8356e073a005de72
Denys Shchablevskyi <dendhg.rgbg@gmail.com>
2024-06-07 00:13:39 +0300

    [3] [FEATURE] crud finalization

---
+++ src/app/shared/location-not-allowed/location-not-allowed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-not-allowed',
+  standalone: true,
+  imports: [],
+  templateUrl: './location-not-allowed.component.html',
+  styleUrl: './location-not-allowed.component.scss'
+})
+export class LocationNotAllowedComponent { }
