971f787dd99455e99c16f56a58392a5df1bcd35e
Denys Shchablevskyi <dendhg.rgbg@gmail.com>
2024-06-10 10:25:03 +0300

    [6] [TASK] env variables

---
+++ src/app/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  WEATHER_API_KEY: '__WEATHER_API_KEY__',
+  WEATHER_API_PATH: '__WEATHER_API_PATH__',
+}
---
+++ src/app/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  WEATHER_API_KEY: '__WEATHER_API_KEY__',
+  WEATHER_API_PATH: '__WEATHER_API_PATH__',
+}
